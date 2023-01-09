d65745a03016802d6e4c2214eb9e58f58ce6188c
Sourdeval <sourdeval@laposte.net>
2023-01-09 01:11:55 +0100

    Tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'; 
 import { HttpClientModule } from '@angular/common/http';
+import { MatTooltipModule } from '@angular/material/tooltip'; 
 
 const ROUTES: Routes = [
   { path: '', component: HomeComponent },
@@ -26,6 +27,7 @@ const ROUTES: Routes = [
     BrowserAnimationsModule,
     MatCardModule,
     MatIconModule,
+    MatTooltipModule,
     HttpClientModule
   ],
   providers: [],
